f303ec20d79813d9d682061351df383c1fadf6ad
Austin Hunt <huntaj@cofc.edu>
2024-01-16 17:09:13 -0500

    create search.php just for native search

--- search.php
+++ search.php
@@ -1,40 +1,23 @@
 <?php
 /**
- * Template Name: Search Page
+ * Template Name: Search Page for Native Search (when ?s= is used)
  */
 get_header();
-
-global $post;
-
-$search_query = isset($_GET['searchwp']) ? sanitize_text_field($_GET['searchwp']) : null;
-$search_page = isset($_GET['swppg']) ? absint($_GET['swppg']) : 1;
-$current_blog_id = get_current_blog_id();
-
-// init search results to empty 
-$search_results = array();
-$search_pagination = '';
-
-if (!empty($search_query) && class_exists('\\SearchWP\\Query')) {
-	$searchwp_query = new \SearchWP\Query($search_query, [
-		'engine' => 'cofcengine', // The Engine name.
-		'fields' => 'all',          // Load proper native objects of each result.
-		'page' => $search_page,
-		'posts_per_page' => get_option('posts_per_page'),
-	]);
-	$search_results = $searchwp_query->get_results();
-
-	$search_pagination = paginate_links(
-		array(
-			'format' => '?swppg=%#%',
-			'current' => $search_page,
-			'total' => $searchwp_query->max_num_pages,
-		)
-	);
-}
-// TODO; use default search if class not defined. 
+$s = get_search_query();
+$paged = (get_query_var('paged')) ? get_query_var('paged') : 1; // Get the current page number  
+$args = array(
+	's' => $s,
+	'paged' => $paged,
+	// pull default posts per page from settings
+	'posts_per_page' => get_option('posts_per_page'),
+	// Set the number of posts per page
+	'orderby' => 'date',
+	'order' => 'DESC'
+);
+// The Query
+$query = new WP_Query($args);
 ?>
 <main id="main" class="search-results">
-	<!-- current blog ID: <?php echo $current_blog_id ?> -->
 	<div class="search-results__wrapper wrapper">
 		<div class="row">
 			<div class="search-results__content xsmall-12 cell">
@@ -43,18 +26,17 @@ if (!empty($search_query) && class_exists('\\SearchWP\\Query')) {
 						<?php printf(esc_html__('Search Results for: %s', 'torba'), '<span>' . get_search_query() . '</span>'); ?>
 					</h1>
 					<hr>
-					<form class="search-results__search" role="search" method="get"
-						action="<?php echo get_site_url() ?>">
+					<form class="search-results__search" method="get" action="<?php echo get_site_url() ?>">
 						<div class="form__field form__field--is-search form__field--is-valid">
-							<input id="searchwp" name="searchwp" type="search"
-								value="<?php echo isset($_GET['searchwp']) ? esc_attr($_GET['searchwp']) : '' ?>"
-								required="required" title="<?php echo esc_attr_x('Search for:', 'label') ?>">
+							<input id="s" name="s" type="search" value="Site search" required="required">
 							<label for="search" style="visibility:hidden;">Search term</label>
 							<button type="submit" class="search-button">
-								<span class="show-for-sr">Search</span>
+								<span class="show-for-sr">Search term</span>
+
 								<svg class="brei-icon brei-icon-search" focusable="false">
 									<use href="#brei-icon-search"></use>
 								</svg>
+
 							</button>
 						</div>
 					</form>
@@ -65,11 +47,11 @@ if (!empty($search_query) && class_exists('\\SearchWP\\Query')) {
 				<div id="results-full-container" class="aggregate__set">
 					<div id="results-info-container" class="row level_search--navigation aggregate__info">
 						<div id="results-count" class="level__pages xsmall-12 large-order-2 column aggregate__count ">
-							<?php if (!empty($search_query)) {
-								echo "Page " . $search_page . " of " . $searchwp_query->max_num_pages;
-							}
-							?>
+							Page
+							<?php echo $paged ?> of
+							<?php echo $query->max_num_pages ?>
 						</div>
+
 					</div>
 
 					<!--div id="results_loader" class="aggregate__loading">
@@ -91,44 +73,23 @@ if (!empty($search_query) && class_exists('\\SearchWP\\Query')) {
 						<section class="news-content">
 							<div class="news-content__cards grid-x grid-margin-x grid-margin-y">
 
-								<?php if (!empty($search_query) && !empty($search_results)) {
-									foreach ($search_results as $search_result) {
-										$post = $search_result;
-										if ($current_blog_id != $post->site) {
-											echo "<!-- current blog id " . $current_blog_id . " NOT EQUAL TO post site ID " . $post->site . "-->";
-											switch_to_blog($post->site);
-											$post = get_post($post->ID);
-											?>
-											<div class="cell xsmall-12 medium-6 large-4">
-												<?php
-												display_featured_post_card($post, wide: false);
-												?>
-											</div>
+								<?php if ($query->have_posts()) {
+									while ($query->have_posts()) {
+										$query->the_post();
+										?>
+										<div class="cell xsmall-12 medium-6 large-4">
 											<?php
-											restore_current_blog();
-										} else {
-											echo "<!-- current blog id " . $current_blog_id . " EQUAL TO post site ID " . $post->site . "-->";
-											$post = get_post($post->ID);
+											display_featured_post_card($post, wide: false);
 											?>
-											<div class="cell xsmall-12 medium-6 large-4">
-												<?php
-												display_featured_post_card($post, wide: false);
-												?>
-											</div>
-											<?php
-										}
+										</div>
+										<?php
 									}
 									wp_reset_postdata(); // At the end reset your query 
 									?>
 									<div class="xsmall-12">
 										<div id="js-pagination" role="navigation" class="pagination aggregate__pagination">
 											<?php
-											echo custom_pagination_links(
-												max_num_pages: $searchwp_query->max_num_pages,
-												current_page: $search_page,
-												base_url: '',
-												query: $search_query
-											);
+											echo custom_pagination_links(max_num_pages: $query->max_num_pages, current_page: $paged, base_url: '', query: $s);
 											?>
 										</div>
 									</div>
@@ -138,8 +99,8 @@ if (!empty($search_query) && class_exists('\\SearchWP\\Query')) {
 									<div class="component">
 										<div class="row level__row">
 											<div class="level__content large-8 large-push-2 columns">
-												<h1 class="page-title">
-													<?php echo "No matching posts found" ?>
+												<h1 class="page-title">No posts were found matching your query "
+													<?php echo $s ?>"
 												</h1>
 											</div>
 										</div>
